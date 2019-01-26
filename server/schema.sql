5f5bad829cc879ac5d2d199f2bf61bc6d9200c63
singer30 <lukas.w.singer@gmail.com>
2019-01-25 21:21:21 -0800

    Create a schema for chat database

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,30 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
-
+/* Describe your table here.*/
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  text VARCHAR(140) NOT NULL,
+  userID INT NOT NULL,
+  roomID INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT,
+  room VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(32) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
 
-
-
-
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
-
+ *  to create the database and the tables.*
