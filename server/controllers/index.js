81f742c55f49d590f0a9e2ba64f0b5841b85c74a
singer30 <lukas.w.singer@gmail.com>
2019-01-26 21:15:29 -0800

    Update the server spec and index.js

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -6,16 +6,16 @@ module.exports = {
   messages: {
     get: function (req, res) {
       models.messages.get(function(err, results) {
-        if (err) {  }
+        if (err) {}
         res.json(results);
-      })
+      });
     }, // a function which handles a get request for all messages
     post: function (req, res) {
-      var parameters = [req.body.message, req.body.roomname, req.body.username];
+      var parameters = [req.body.text, req.body.roomname, req.body.username];
       models.messages.post(parameters, function(err, results) {
-           if(err) { }
-           res.json(results);
-      })
+        if (err) { }
+        res.sendStatus(201);
+      });
    
     } // a function which handles posting a message to the database
   },
@@ -24,18 +24,18 @@ module.exports = {
     // Ditto as above
     get: function (req, res) {
       models.users.get(function(err, results) {
-        if (err) {  }
+        if (err) {}
         res.json(results);
-      })
+      });
     },
 
 
     post: function (req, res) {
       var parameters = [req.body.username];
       models.users.post(parameters, function(err, results) {
-           if(err) { }
-           res.json(results);
-      })
+        if(err) {}
+        res.sendStatus(201);
+      });
     }
   }
 };
